31a73ef2393810ff4aff665f59b7e4b45dd78103
johnwonder <johnufida@163.com>
2015-09-16 14:51:51 +0000

    状态栏学习

--- mytheme/src/main/java/com/ufida/john/mytheme/MainActivity.java
+++ mytheme/src/main/java/com/ufida/john/mytheme/MainActivity.java
@@ -1,9 +1,12 @@
-package com.ufida.john.mygreendao;
+package com.ufida.john.mytheme;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        SystemBarTintManager tintManager = new SystemBarTintManager(this);
+        tintManager.setStatusBarTintResource(R.color.colorPrimary);
+        tintManager.setStatusBarTintEnabled(true);
     }
 
     @Override
