bf613babae244ba4fec7e5384c213c8b78d539b0
johnwonder <johnufida@163.com>
2015-09-17 15:21:44 +0000

    toobar学习 颜色貌似改不过来 啊

--- mytheme/src/main/java/com/ufida/john/mytheme/MainActivity.java
+++ mytheme/src/main/java/com/ufida/john/mytheme/MainActivity.java
@@ -1,33 +1,43 @@
 package com.ufida.john.mytheme;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ToolBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
+    }
 
-        SystemBarTintManager tintManager = new SystemBarTintManager(this);
-        tintManager.setStatusBarTintResource(R.color.colorPrimary);
-        tintManager.setStatusBarTintEnabled(true);
+    @Override
+    public void onCreateCustomToolBar(Toolbar toolbar) {
+        super.onCreateCustomToolBar(toolbar);
+        toolbar.showOverflowMenu() ;
+        getLayoutInflater().inflate(R.layout.toobar_button, toolbar) ;
+//        TextView btn = (TextView) toolbar.findViewById(R.id.id_txt_btn);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(v.getContext(),"test",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        MenuItem itemCompat = menu.findItem(R.id.action_search) ;
+//        SearchView mSearchView = (SearchView) MenuItemCompat.getActionView(itemCompat);
+//        mSearchView.setIconified(false);
+//        mSearchView.setIconifiedByDefault(false);
         return true;
     }
 
@@ -40,6 +50,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
