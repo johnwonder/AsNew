ef05d769df897bcd4ab9c3f1b90febc789a9469c
johnwonder <johnufida@163.com>
2015-10-11 13:58:36 +0000

    drawerLayout 左右菜单

---
+++ mydrawerlayout2/src/main/java/com/ufida/john/mydrawerlayout2/OtherActivity.java
@@ -0,0 +1,16 @@
+package com.ufida.john.mydrawerlayout2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jay on 2015/10/9 0009.
+ */
+public class OtherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
---
+++ mydrawerlayout2/src/main/java/com/ufida/john/mydrawerlayout2/RightFragment.java
@@ -0,0 +1,58 @@
+package com.ufida.john.mydrawerlayout2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by john on 2015/10/11.
+ */
+public class RightFragment  extends Fragment implements View.OnClickListener {
+
+    private DrawerLayout drawer_layout;
+    private FragmentManager fManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg_right, container, false);
+        view.findViewById(R.id.btn_one).setOnClickListener(this);
+        view.findViewById(R.id.btn_two).setOnClickListener(this);
+        view.findViewById(R.id.btn_three).setOnClickListener(this);
+        fManager = getActivity().getSupportFragmentManager();
+        return  view;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_one:
+                ContentFragment cFragment1 = new ContentFragment("1.点击了右侧菜单项一",R.color.blue);
+                fManager.beginTransaction().replace(R.id.fly_content,cFragment1).commit();
+                drawer_layout.closeDrawer(Gravity.END);
+                break;
+            case R.id.btn_two:
+                ContentFragment cFragment2 = new ContentFragment("2.点击了右侧菜单项二",R.color.red);
+                fManager.beginTransaction().replace(R.id.fly_content,cFragment2).commit();
+                drawer_layout.closeDrawer(Gravity.END);
+                break;
+            case R.id.btn_three:
+                ContentFragment cFragment3 = new ContentFragment("3.点击了右侧菜单项三",R.color.yellow);
+                fManager.beginTransaction().replace(R.id.fly_content,cFragment3).commit();
+                drawer_layout.closeDrawer(Gravity.END);
+                break;
+        }
+    }
+
+    public  void setDrawerLayout(DrawerLayout drawer_layout)
+    {
+        this.drawer_layout = drawer_layout;
+    }
+}
