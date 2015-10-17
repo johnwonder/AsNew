7c7f046a43bf06d58041bf7faa9dc1a9f1f4297e
johnwonder <johnufida@163.com>
2015-10-17 12:21:59 +0000

    EventBus 初次使用  register unregister onEventMainThread

---
+++ myeventbus/src/main/java/com/ufida/john/myeventbus/FirstEvent.java
@@ -0,0 +1,16 @@
+package com.ufida.john.myeventbus;
+
+/**
+ * Created by john on 2015/10/17.
+ */
+public class FirstEvent {
+    private  String mMsg;
+
+    public FirstEvent(String mMsg) {
+        this.mMsg = mMsg;
+    }
+
+    public String getmMsg() {
+        return mMsg;
+    }
+}
